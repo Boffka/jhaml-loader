b5f07bd769cac970cd6fbbf78b3cfd013cdf0bc0
Gaël Moliner <gmoliner@ds-restauration.com>
2016-08-23 12:15:43 +0200

    fix(index.js): missing fs

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+var fs = require('fs')
+
 module.exports = function (source) {
-  var loaderUtils, jhaml, query, result
+  let loaderUtils, jhaml, query, result
 
   this.cacheable && this.cacheable(true)
   loaderUtils = require("loader-utils")
   jhaml = require('@soyuka/jhaml')
   query = loaderUtils.parseQuery(this.query)
 
-  var callback = this.async();
-
-
+  let callback = this.async()
   let engine = jhaml()
+
   fs.createReadStream(source)
   .pipe(engine)
 
@@ -25,7 +26,4 @@ module.exports = function (source) {
   engine.on('end', function() {
     callback(null, Buffer.concat(chunks).toString())
   })
-
-
-
 }
