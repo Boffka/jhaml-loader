3e08c5a08e33015180bda0cf6f47a20e45925d41
Gaël Moliner <gmoliner@ds-restauration.com>
2016-08-23 11:58:27 +0200

    fix(jhaml-loader): change html to haml

--- index.js
+++ index.js
@@ -3,13 +3,29 @@ module.exports = function (source) {
 
   this.cacheable && this.cacheable(true)
   loaderUtils = require("loader-utils")
-  jhaml = require('jhaml')
+  jhaml = require('@soyuka/jhaml')
   query = loaderUtils.parseQuery(this.query)
-  try {
-    result = jhaml.render(source, query)
-  } catch (e) {
-    this.emitError('JHAML:' + e)
-    throw e
-  }
-  return "module.exports = " + JSON.stringify(result) + ";"
+
+  var callback = this.async();
+
+
+  let engine = jhaml()
+  fs.createReadStream(source)
+  .pipe(engine)
+
+  let chunks = []
+  engine.on('data', function(str) {
+    chunks.push(str)
+  })
+
+  engine.on('error', function(err) {
+    callback(err)
+  })
+
+  engine.on('end', function() {
+    callback(null, Buffer.concat(chunks).toString())
+  })
+
+
+
 }
